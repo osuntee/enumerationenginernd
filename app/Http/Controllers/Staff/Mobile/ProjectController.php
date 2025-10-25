550d7903a5c27d6896a18bdca2b7684580d94f72
Gsent10 <78319551+Gsent10@users.noreply.github.com>
2025-10-25 07:19:45 +0100

    Update ProjectController.php

--- app/Http/Controllers/Staff/Mobile/ProjectController.php
+++ app/Http/Controllers/Staff/Mobile/ProjectController.php
@@ -154,7 +154,7 @@ class ProjectController extends Controller
 
     public function verify($ref)
     {
-        $activities = Activity::where('activity_type', 'Verification')->delete();
+        Activity::where('activity_type', 'Verification')->delete();
 
         $enumeration = Enumeration::where('reference', $ref)
             ->with('enumerationData.projectField')
