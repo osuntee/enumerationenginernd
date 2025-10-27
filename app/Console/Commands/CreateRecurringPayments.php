1eda9c38b84139d948f0a3396d9bd91e17b8c3ab
Gsent10 <78319551+Gsent10@users.noreply.github.com>
2025-10-27 12:34:25 +0100

    Create CreateRecurringPayments.php

---
+++ app/Console/Commands/CreateRecurringPayments.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class CreateRecurringPayments extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:create-recurring-payments';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        //
+    }
+}
