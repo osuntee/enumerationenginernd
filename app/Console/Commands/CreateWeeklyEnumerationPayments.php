4e64c3e5976b598bc27322943e1ac8733c290e94
Gsent10 <78319551+Gsent10@users.noreply.github.com>
2025-10-30 12:48:37 +0100

    Create CreateWeeklyEnumerationPayments.php

---
+++ app/Console/Commands/CreateWeeklyEnumerationPayments.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class CreateWeeklyEnumerationPayments extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:create-weekly-enumeration-payments';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        //
+    }
+}
