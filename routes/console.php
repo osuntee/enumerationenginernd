d2290b8e28df7b23601bd03dd2f6c0cc0c36ced5
Gsent10 <78319551+Gsent10@users.noreply.github.com>
2025-10-30 14:12:26 +0100

    Update console.php

--- routes/console.php
+++ routes/console.php
@@ -1,13 +1,3 @@
 <?php
 
 use Illuminate\Support\Facades\Schedule;
-use Illuminate\Foundation\Inspiring;
-use Illuminate\Support\Facades\Artisan;
-
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote');
-
-Schedule::call(function () {
-    \Log::info('⚡ Scheduler test executed successfully at ' . now());
-})->everyMinute();
