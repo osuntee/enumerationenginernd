abb587e9d66ace3cb9499381a867bf8a15f5ec40
Gsent10 <78319551+Gsent10@users.noreply.github.com>
2026-01-02 18:48:05 +0100

    Update show.blade.php

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -170,12 +170,12 @@
                 <div class="p-6 text-gray-900">
                     <h3 class="text-lg font-medium text-gray-900 mb-3">Project Utility Links</h3>
 
-                    <div class="flex flex-row items-centre justify-start">
-                        <p class="text-xs gap-1">
+                    <div class="flex flex-row items-centre justify-start gap-1">
+                        <p class="text-xs">
                             Self Enumeration URL: 
                         </p>
                         <div class="flex flex-row items-centre gap-3">
-                            <p class="text-xs font-medium">
+                            <p class="text-sm font-medium">
                                 {{ config('app.url') }}/enumerate/{{ $project->code }}
                             </p>
                             <button
