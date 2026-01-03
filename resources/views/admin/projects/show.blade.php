2a1de35a3bed44ae32f3e691220febf95c4973f9
Gsent10 <78319551+Gsent10@users.noreply.github.com>
2026-01-03 10:00:03 +0100

    Update show.blade.php

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -234,7 +234,7 @@
                     </div>
                     @if ($project->allow_api)
                         <p class="text-sm font-medium">
-                            {{ config('app.url') }}/api/enumerate/{{ $project->code }}
+                            Usage Notes: The reference field is always required when submitting data via the API. Form field validation follows the same rules as defined in the Project Fields.
                         </p>
                     @endif
                 </div>
