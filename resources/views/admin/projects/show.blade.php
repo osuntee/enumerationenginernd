773edd68e16a81fd209f9fa2b90b57a6640c8166
Gsent10 <78319551+Gsent10@users.noreply.github.com>
2026-01-03 10:35:05 +0100

    Update show.blade.php

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -234,8 +234,13 @@
                     </div>
                     @if ($project->allow_api)
                         <p class="text-xs mt-3">
-                            API Usage Notes: The reference field is always required when submitting data via the API. Form field validation follows the same rules as defined in the Project Fields.
+                            API Usage Notes:
                         </p>
+                        <ul>
+                            <li>The reference field is always required when submitting data via the API</li>
+                            <li>Form field validation follows the same rules as defined in the Project Fields.</li>
+                            <li>Ensure every project field is preceeded with "data."</li>
+                        </ul>
                     @endif
                 </div>
             </div>
