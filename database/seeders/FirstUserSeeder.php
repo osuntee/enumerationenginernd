9ea1700bb0f01bd2248607ee4da1aec7a03c8bb1
Gsent10 <78319551+Gsent10@users.noreply.github.com>
2026-01-05 12:54:00 +0100

    Create FirstUserSeeder.php

---
+++ database/seeders/FirstUserSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class FirstUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
