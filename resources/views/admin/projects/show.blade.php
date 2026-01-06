6205c20f0009c6452757c573113c169237c123bd
Gsent10 <78319551+Gsent10@users.noreply.github.com>
2026-01-06 12:58:06 +0100

    Update show.blade.php

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -165,7 +165,7 @@
                 </div>
             </div>
             
-            <!-- Project URL -->
+            <!-- Project URLs -->
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg mb-6">
                 <div class="p-6 text-gray-900">
                     <h3 class="text-lg font-medium text-gray-900 mb-3">Project Utility Links</h3>
