89e35fed17f8d42152d29b4f19cd30ab4183e635
Gsent10 <78319551+Gsent10@users.noreply.github.com>
2026-01-07 12:24:50 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -100,7 +100,7 @@ Route::prefix('admin')->middleware('auth')->group(function () {
         Route::patch('projects/enumerations/{enumeration}/verify', [EnumerationController::class, 'toggleVerification'])->name('enumeration.toggleVerification');
         Route::get('projects/{project}/export', [EnumerationController::class, 'export'])->name('enumeration.export');
 
-        Route::get('codes/{project}', [EnumerationController::class, 'export'])->name('codes.index');
+        Route::get('codes/{project}', [CodeController::class, 'index'])->name('codes.index');
     });
 
     Route::prefix('admins')->name('admins.')->group(function () {
