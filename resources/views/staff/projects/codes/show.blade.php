e2501ccb2a5ee174163b49c81d5f4395cfb56598
Gsent10 <78319551+Gsent10@users.noreply.github.com>
2026-02-15 09:40:49 +0100

    Update show.blade.php

--- resources/views/staff/projects/codes/show.blade.php
+++ resources/views/staff/projects/codes/show.blade.php
@@ -12,7 +12,7 @@
                     <svg class="w-4 h-4 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 17h2a2 2 0 002-2v-4a2 2 0 00-2-2H5a2 2 0 00-2 2v4a2 2 0 002 2h2m2 4h6a2 2 0 002-2v-4a2 2 0 00-2-2H9a2 2 0 00-2 2v4a2 2 0 002 2zm8-12V5a2 2 0 00-2-2H9a2 2 0 00-2 2v4h10z"/></svg>
                     Print QR Labels
                 </button>
-                <a href="{{ route('projects.codes.index', $batch->project) }}" class="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded">
+                <a href="{{ route('staff.projects.codes.index', $batch->project) }}" class="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded">
                     Back to Batches
                 </a>
             </div>
