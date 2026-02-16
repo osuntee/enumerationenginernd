9640d592bef5842d92b89e93d68284225208467b
Gsent10 <78319551+Gsent10@users.noreply.github.com>
2026-02-16 15:12:20 +0100

    Update index.blade.php

--- resources/views/admin/projects/codes/index.blade.php
+++ resources/views/admin/projects/codes/index.blade.php
@@ -124,9 +124,6 @@
                                                         <a href="{{ route('projects.codes.show', [$project, $batch]) }}" class="inline-flex items-center px-3 py-1.5 bg-white border border-gray-300 rounded-md font-semibold text-xs text-gray-700 uppercase tracking-widest shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 disabled:opacity-25 transition ease-in-out duration-150">
                                                             View Codes
                                                         </a>
-                                                        <a href="{{ route('projects.codes.show', ['project' => $project, 'batch' => $batch, 'print' => 1]) }}" target="_blank" class="inline-flex items-center px-3 py-1.5 bg-indigo-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-indigo-700 focus:bg-indigo-700 active:bg-indigo-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">
-                                                            Print PDF
-                                                        </a>
                                                     </div>
                                                     <div x-show="status !== 'completed' && status !== 'failed'">
                                                         <span class="inline-flex items-center px-3 py-1.5 bg-gray-100 border border-transparent rounded-md font-semibold text-xs text-gray-400 uppercase tracking-widest cursor-not-allowed">
